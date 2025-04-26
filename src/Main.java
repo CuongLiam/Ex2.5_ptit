8e4aaa871436c4e870573577ed09e659043ac2d0
CuongLiam <cuongdeptrai2706@gmail.com>
2025-04-27 00:04:59 +0700

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+// Hãy viết chương trình cho phép nhập vào các chuỗi, tính và xuất ra
+// tổng chiều dài của các chuỗi có chứa ký tự in hoa.
+// Nếu người dùng nhập vào chuỗi rỗng thì kết thúc chương trình.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int totalLength = 0;
+
+        System.out.println("Nhập chuỗi (nhập chuỗi rỗng để kết thúc):");
+
+        while (true) {
+            String str = scanner.nextLine();
+
+            if (str.isEmpty()) {
+                break; // end input when empty
+            }
+
+            boolean flag = false;
+
+            for (int j = 0; j < str.length(); j++) {
+                char ch = str.charAt(j);
+                if (ch >= 'A' && ch <= 'Z') {
+                    flag = true;
+                    break; // we found an uppercase, no need to check more
+                }
+            }
+
+            if (flag) {
+                totalLength += str.length();
+            }
         }
+
+        System.out.println(totalLength);
     }
 }
